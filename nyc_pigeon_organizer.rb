3e76df89d49babf7dc57d4619c9fd4ce73880ee6
Radouane Bahi <radbahi@gmail.com>
2019-12-04 17:17:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  
+ 
+  pigeonList = {}
+  data.each do |attributesList, specificAttributes|
+    value.each do |specificAttributes, pigeonNames|
+      pigeonNames.each do |specificPigeon|
+        if pigeonList[specificPigeon] == nil
+          pigeon_list[specificPigeon] = {}
+        end
+        if pigeonList[specificPigeon][attributesList] == nil
+          pigeonList[specificPigeon][attributesList] = []
+        end
+        pigeonList[specificPigeon][attributesList].push(specificAttributes.to_s)
+      end
+    end 
+  end
+  pigeonList
 end
